cf8b9ed18d6368507896738a206af16915efdd93
Thomas James <tomsterlingjames@outlook.com.au>
2022-06-11 12:57:04 +1000

    forgot that switch doesnt work with routes anymore so its been changed to Routes and elements as per the v6

--- visualsbt/src/App.js
+++ visualsbt/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Home from './pages';
 import ContactPage from './pages/contact';
@@ -9,11 +9,10 @@ import ContactPage from './pages/contact';
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/contact' component={ContactPage} exact />
-      </Switch>
-     
+     <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path='/contact' element={<ContactPage />} />
+        </Routes>
     </Router>
   );
 }
--- visualsbt/src/pages/contact.js
+++ visualsbt/src/pages/contact.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 
+
+
 const ContactPage = () => {
   return (
     <div>
-      <Navbar />
+     <Navbar />
         <h1>Contact Me</h1>
     </div>
   )
