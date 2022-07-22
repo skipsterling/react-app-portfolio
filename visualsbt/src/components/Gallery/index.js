3fded2a7248f3be819b2cdb0e63580c0c4867446
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-07-22 15:40:31 +1000

    Images added from google drive

--- visualsbt/src/components/Gallery/index.js
+++ visualsbt/src/components/Gallery/index.js
@@ -37,10 +37,10 @@ const PhotoGallery = () => {
      </Container>
      <Container>
        
-       <Column1><Img src={Img1} /></Column1>
-       <Column2><Img src={Img2}/></Column2>      
-       <Column3><Img src={Img3}/></Column3>     
-       <Column4><Img src={Img4} /></Column4>
+       <Column1><Img src='https://drive.google.com/uc?export=view&id=1FiTjEDZgeBW9T7WNqWsa-dksU7L4tMhX' /></Column1>
+       <Column2><Img src='https://drive.google.com/uc?export=view&id=1fuCA7c-kt72pdmJbqNimvtXokINyUHjT'/></Column2>      
+       <Column3><Img src='https://drive.google.com/uc?export=view&id=1-9yfSWxHXLgMhlt6XVAueK34KxiUvUaC'/></Column3>     
+       <Column4><Img src='https://drive.google.com/uc?export=view&id=1NPVzKF-KDdWyU_K8Y1rIbjMmUkimtK_9' /></Column4>
        
      </Container>
     </div>
