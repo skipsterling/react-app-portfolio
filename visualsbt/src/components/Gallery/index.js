8b61f8ede1194ba990362cf02da3b3c8c3ea93b7
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-07-26 18:52:12 +1000

    more images added to gallery and placement changed

--- visualsbt/src/components/Gallery/index.js
+++ visualsbt/src/components/Gallery/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Img1 from '../../images/1.jpg';
-import Img2 from '../../images/2.jpg';
-import Img3 from '../../images/5.jpg';
-import Img4 from '../../images/4.jpg';
+// import Img2 from '../../images/2.jpg';
+import Img9 from '../../images/9.jpg';
+import Img10 from '../../images/10.jpg';
 
 import {
   Container,
@@ -20,26 +20,27 @@ const PhotoGallery = () => {
     <div>
       <Header>IMAGES</Header>
       <Container>
-       
+      
+      
         <Column1><Img src={Img1} /></Column1>
-        <Column2><Img src={Img2}/></Column2>      
-        <Column3><Img src={Img3}/></Column3>     
-        <Column4><Img src={Img4} /></Column4>
+        <Column2><Img src={Img10}/></Column2>      
+        <Column3><Img src={Img9}/></Column3>     
+        <Column4><Img src='https://drive.google.com/uc?export=view&id=1rOxu-mh1tvm-SgzbHclKcrFF1Ybbqf4d' /></Column4>
         
       </Container>
       <Container>
        
-       <Column1><Img src={Img1} /></Column1>
-       <Column2><Img src={Img2}/></Column2>      
-       <Column3><Img src={Img3}/></Column3>     
-       <Column4><Img src={Img4} /></Column4>
+       <Column1><Img src='https://drive.google.com/uc?export=view&id=11O8QGecU3pVJrBc7FYMCsQfVbPVDrc5d' /></Column1>
+       <Column2><Img src='https://drive.google.com/uc?export=view&id=1hHHAkyIYZ3SWSWfD8rfAT-tiEFihA70h'/></Column2>      
+       <Column3><Img src='https://drive.google.com/uc?export=view&id=1YAuWf5fJ886l5kyH5QMu8aOPXppXYTpe'/></Column3>     
+       <Column4><Img src='https://drive.google.com/uc?export=view&id=1y-DTYhrP81Z5k4YGGly1MSRJJPAivaLl' /></Column4>
        
      </Container>
      <Container>
-       
-       <Column1><Img src='https://drive.google.com/uc?export=view&id=1FiTjEDZgeBW9T7WNqWsa-dksU7L4tMhX' /></Column1>
-       <Column2><Img src='https://drive.google.com/uc?export=view&id=1fuCA7c-kt72pdmJbqNimvtXokINyUHjT'/></Column2>      
-       <Column3><Img src='https://drive.google.com/uc?export=view&id=1-9yfSWxHXLgMhlt6XVAueK34KxiUvUaC'/></Column3>     
+     
+       <Column1><Img src='https://drive.google.com/uc?export=view&id=1fuCA7c-kt72pdmJbqNimvtXokINyUHjT' /></Column1>
+       <Column2><Img src='https://drive.google.com/uc?export=view&id=1-9yfSWxHXLgMhlt6XVAueK34KxiUvUaC'/></Column2>      
+       <Column3><Img src='https://drive.google.com/uc?export=view&id=1T0HIkgbH_nYb6DqQ0MgZDab0YCEGxB_3'/></Column3>     
        <Column4><Img src='https://drive.google.com/uc?export=view&id=1NPVzKF-KDdWyU_K8Y1rIbjMmUkimtK_9' /></Column4>
        
      </Container>
