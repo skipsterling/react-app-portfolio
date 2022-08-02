69b4741b39294f33b0f7aaa4644b973706d035d9
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-08-02 19:35:18 +1000

    more images added

--- visualsbt/src/components/WeddingGallery/index.js
+++ visualsbt/src/components/WeddingGallery/index.js
@@ -19,15 +19,15 @@ import {
         
           <Column1><Img src='https://drive.google.com/uc?export=view&id=1qW8kuE2TVyqkGk8r-Oaslwy2fHokEGA7' /></Column1>
           <Column2><Img src='https://drive.google.com/uc?export=view&id=1cu-CyXKIRg3iae1VP0ibbNfjKq5OrXPg'/></Column2>      
-          <Column3><Img src=''/></Column3>     
-          <Column4><Img src='' /></Column4>
+          <Column3><Img src='https://drive.google.com/uc?export=view&id=18hp7cSbXxQ_oOTBQ-gfT_KFBOD4nbTBZ'/></Column3>     
+          <Column4><Img src='https://drive.google.com/uc?export=view&id=1TWRETjdbTFS0Mwy1pJGZW-DDplGmLW8r'/></Column4>
           
         </Container>
         <Container>
          
-         <Column1><Img src='' /></Column1>
-         <Column2><Img src=''/></Column2>      
-         <Column3><Img src=''/></Column3>     
+         <Column1><Img src='https://drive.google.com/uc?export=view&id=1bYgewgbvJFgkXYj8UIN4XhYYpUDax6cT' /></Column1>
+         <Column2><Img src='https://drive.google.com/uc?export=view&id=1Lr4XBZzIk1vUeEb5rzCdNG5Zi7frJOWL'/></Column2>      
+         <Column3><Img src='https://drive.google.com/uc?export=view&id=1XXJYhxhDNsB_e_Pz1SCoPuP09z9ntGmC'/></Column3>     
          <Column4><Img src='' /></Column4>
          
        </Container>
