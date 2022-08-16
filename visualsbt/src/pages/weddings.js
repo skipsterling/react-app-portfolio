0a51c96bd10cb90b3da3d0b72ea116f299ae9f58
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-08-16 18:50:29 +1000

    video being added to weddings page

--- visualsbt/src/pages/weddings.js
+++ visualsbt/src/pages/weddings.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Weddings from '../components/Weddings';
+import VidWedOne from '../components/VidWedOne.js';
 
 const WeddingsPage = () => {
   return (
     <div>
       <Weddings />
-
+      <VidWedOne />
     </div>
   )
 }
