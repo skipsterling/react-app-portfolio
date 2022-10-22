6c4d26429409682d79884a03eaf517faeb8d3a18
Thomas James <90020530+skipsterling@users.noreply.github.com>
2022-10-22 12:39:39 +1000

    About page structure and styled elements set up

---
+++ src/components/About/AboutElements.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const AboutContainer = styled.div`
+`;
+
+export const AboutWrapper = styled.div`
+`;
+
+export const AboutRow = styled.div`
+`;
+
+export const ColumnOne = styled.div`
+`;
+
+export const TextWrapper = styled.div`
+`;
+
+export const TopLine = styled.p`
+`;
+
+export const Header = styled.h1`
+`;
+
+export const Subtitle = styled.p`
+`;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,7 +3,22 @@ import React from 'react'
 const aboutComp = () => {
   return (
     <div>
-      about
+      <AboutContainer>
+        <AboutWrapper>
+          <AboutRow>
+            <ColumnOne>
+            <TextWrapper>
+              <TopLine>About Me</TopLine>
+              <Header>Thomas Sterling James</Header>
+              <Subtitle>My about me section will go here.</Subtitle>
+            </TextWrapper>
+            </ColumnOne>
+            <ColumnTwo>
+            
+            </ColumnTwo>
+          </AboutRow>
+        </AboutWrapper>
+      </AboutContainer>
       </div>
   )
 }
