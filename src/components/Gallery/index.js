a855ca8de33e73fbddbf7dae55b358749a9b63a2
Thomas James <90020530+skipsterling@users.noreply.github.com>
2023-01-17 07:57:00 +1000

    h2 heading for cosmeditour seq

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -50,7 +50,7 @@ const PhotoGallery = () => {
        <Column4><Img src='https://drive.google.com/uc?export=view&id=1NPVzKF-KDdWyU_K8Y1rIbjMmUkimtK_9' /></Column4>
        
      </Container>
-
+<h2>CosMediTour Series</h2>
     <Container>
      
        <Column1><Vid src={CMTS1} autoPlay loop muted></Vid></Column1>
